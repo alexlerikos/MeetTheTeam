4b96e7cba5f2b9b089e6eb0265ab3bcc67326c1d
Alex Lerikos <alerikos@gmail.com>
2017-03-04 18:30:03 -0800

    Set up json data loading

--- MeetTheTeam/MeetTheTeam/MTTMainViewController.swift
+++ MeetTheTeam/MeetTheTeam/MTTMainViewController.swift
@@ -21,11 +21,8 @@ class MTTMainViewController: UIViewController {
 		
 		self.view.backgroundColor = UIColor.white
 		self.navigationItem.title = "Meet The Team"
-		
 		self.setupMainTableView()
-		
 		self.setupTeamMemberData()
-		
     }
 
     override func didReceiveMemoryWarning() {
@@ -37,17 +34,27 @@ class MTTMainViewController: UIViewController {
 	
 	private func setupTeamMemberData(){
 		
+		guard let path = getJsonPath() else {
+			return
+		}
 		
-		let member = Mapper<MTTMember>().map(JSON: [:])
-		
-		member?.avatarImageURL = "https://d1qb2nb5cznatu.cloudfront.net/users/114349-large?1462395805"
-		member?.bio = "I have zero cycles for this. What do you feel you would bring to the table if you were hired for this position paddle on both sides, and to be inspired is to become creative, innovative and energized we want this philosophy to trickle down to all our stakeholders but hammer out herding cats. Going forward drop-dead date.\n\nI also believe it's important for every member to be involved and invested in our company and this is one way to do so out of the loop, but beef up, but hit the ground running, or it just needs more cowbell. We're ahead of the curve on that one innovation is hot right now shotgun approach. Value-added take five, punch the tree, and come back in here with a clear head we need to start advertising on social media."
-		member?.firstName = "Sherrie"
-		member?.title = "Chief Produect Officer"
-		member?.lastName = "Chen"
-		member?.id = 1
-		
-		self.teamMemberArray = [member!,member!,member!,member!,member!,member!,member!,member!,member!,member!,member!,member!,member!,member!,member!,member!,member!,member!]
+		guard let jsonData = NSData(contentsOfFile: path) else{
+			return
+		}
+		do {
+			let json:Any! = try JSONSerialization.jsonObject(with: jsonData as Data, options: .mutableContainers)
+			self.teamMemberArray = Mapper<MTTMember>().mapArray(JSONArray: json as! [[String : Any]])
+			self.tableView?.reloadData()
+		} catch {
+			return
+		}
+	}
+	
+	private func getJsonPath() -> String? {
+		guard let path = Bundle.main.path(forResource: "team", ofType: "json") else {
+			return nil
+		}
+		return path
 	}
     
 
